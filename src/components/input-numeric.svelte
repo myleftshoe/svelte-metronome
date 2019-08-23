58fdfcddfea7d3314fb50f2429abda19113a2dff
Paul Sammut <paul@pop-os.localdomain>
2019-08-23 15:00:48 +1000

    robust validation on numeric input

--- src/components/input-numeric.svelte
+++ src/components/input-numeric.svelte
@@ -1,30 +1,46 @@
 <script>
+    import {afterUpdate} from 'svelte';
     export let value = 5;
     export let step = 1;
     export let min = 0;
     export let max = 10;
+    let node;
 
     function decrement() {
-        value = value - step; 
-        if (value < min)
-            value = min;
+        setValue(value - step); 
     }
 
     function increment() {
-        value = value + step; 
-        if (value > max)
+        setValue(value + step); 
+    }
+
+    function setValue(newValue) {
+        if (newValue < min)
+            value = min;
+        else if (newValue > max)
             value = max;
+        else
+            value = newValue;
     }
 
-    function handleChange(e) {
-        value = parseInt(e.target.value);
+    function handleInput(e) {
+        let newValue = parseInt(e.target.value);
+        if (isNaN(newValue))
+            newValue = min;
+        setValue(parseInt(newValue));
+        node = e.target;
     }
 
+    afterUpdate(() => {
+        if (value === min && node)
+            node.select();
+        node = undefined; 
+    })
 </script>
 
 <div>
     <button on:click={decrement} value={value}>-</button>
-    <input type='number' min={min} max={max} bind:value/>
+    <input type='number' min={min} max={max} bind:value on:input={handleInput}/>
     <button on:click={increment} value={value}>+</button>
 </div>
 
