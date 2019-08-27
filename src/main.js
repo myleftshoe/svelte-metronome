87cdafed84eecb8b03ad0cd666355753d7fe0be1
Paul Sammut <paul@pop-os.localdomain>
2019-08-27 18:30:05 +1000

    PWA Working

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import App from './App.svelte';
 
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('/service-worker.js')
+			.then((reg) => {
+			console.log('Service worker registered.', reg);
+			});
+		});
+}
+
 const app = new App({
 	target: document.body,
 });
