75e8afedd78bcdfbd081c0d4d50f856b4bd51fd4
Paul Sammut <paul@pop-os.localdomain>
2019-08-30 15:01:27 +1000

    minor

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-export {default as BeatsControl} from './beats-control';
-export {default as CircularSlider} from './circular-slider';
-export {default as BpmControl} from './bpm-control.svelte';
-export {default as ClicksControl} from './clicks-control.svelte';
-export {default as NumericInput} from './numeric-input.svelte';
-export {default as PlayButton} from './play-button.svelte';
-export {default as Crementor} from './crementor.svelte';
+export { default as BeatsControl } from './beats-control';
+export { default as CircularSlider } from './circular-slider';
+export { default as BpmControl } from './bpm-control.svelte';
+export { default as ClicksControl } from './clicks-control.svelte';
+export { default as NumericInput } from './numeric-input.svelte';
+export { default as PlayButton } from './play-button.svelte';
+export { default as Crementor } from './crementor.svelte';
