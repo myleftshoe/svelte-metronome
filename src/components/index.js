1047259f2b510d9d2f4fe21fe65e615798cfc85c
Paul Sammut <paul@pop-os.localdomain>
2019-08-31 20:39:49 +1000

    Deleted redundant files for this branch

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,5 @@
 export { default as BeatsControl } from './beats-control';
 export { default as Slider } from './slider.svelte';
-export { default as BpmControl } from './bpm-control.svelte';
-export { default as ClicksControl } from './clicks-control.svelte';
-export { default as NumericInput } from './numeric-input.svelte';
 export { default as PlayButton } from './play-button.svelte';
 export { default as Crementor } from './crementor.svelte';
 export { default as Flex } from './flex.svelte';
