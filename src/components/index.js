1a517fc9dacb82d6818c730113f0b7c748101a03
Paul Sammut <paul@pop-os.localdomain>
2019-09-04 10:28:43 +1000

    Removed flex.svelte

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export { default as BeatsControl } from './beats-control';
 export { default as PlayButton } from './play-button.svelte';
-export { default as Flex } from './flex.svelte';
 export { default as Beats } from './beats';
 export { default as Underlay } from './underlay.svelte';
 export { default as Topbar } from './topbar.svelte';
