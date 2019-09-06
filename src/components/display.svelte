f97f4d605b886bea86667e1d99e1b80e2988be7c
Paul Sammut <paul@pop-os.localdomain>
2019-09-06 21:25:36 +1000

    Fixes inability to scroll left when flex justify-content = center

--- src/components/display.svelte
+++ src/components/display.svelte
@@ -16,16 +16,23 @@
         overflow-y: hidden;
         /* transition:all .5s ease; */
     }
+    .justifyleft { 
+        justify-content:flex-start;
+    }
 </style>
 
 <script>
     export let pattern = [];
 
+    import { afterUpdate } from 'svelte'; 
     import { fade } from 'svelte/transition'; 
     import Bar from './bar.svelte';
     import Logo from './logo.svelte';
     
     let showLogo = true;
+    let node;
+    let justifyleft = false;
+
     function toggleBar(index) {
         pattern[index] = !pattern[index];
         pattern=[...pattern];
@@ -35,12 +42,29 @@
         showLogo = true;
     }
 
+    // Fixes inability to scroll left when flex justify-content = center
+    afterUpdate(() => {
+        if (!node) return;
+        if (node.scrollWidth > node.offsetWidth) {
+            node.classList.add('justifyleft');
+            node.lastChild.scrollIntoView({block:'center', inline:'center', behaviour:'smooth'});
+        }
+        else {
+            node.classList.remove('justifyleft');
+        }
+    });
+
     $: showLogo = showLogo && !pattern.length;
 </script>
 
 <div class='container' on:click>
     {#if pattern.length}
-        <div class=pattern transition:fade on:outroend={handleOutroEnd}>
+        <div bind:this={node} 
+            class=pattern 
+            class:justifyleft 
+            transition:fade 
+            on:outroend={handleOutroEnd}
+        >
             {#each pattern as big, id}
                 <Bar {id} {big} on:click={() => toggleBar(id)}/>
             {/each}
