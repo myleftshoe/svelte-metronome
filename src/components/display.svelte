e8e189b08f1d849e3fb1f5aa3452776afce3aafb
Paul Sammut <paul@pop-os.localdomain>
2019-09-06 19:02:05 +1000

    minor tweak

--- src/components/display.svelte
+++ src/components/display.svelte
@@ -10,7 +10,7 @@
         display:flex;
         justify-content:center;
         align-items:center; 
-        max-width:100%;
+        width:100%;
         height:100%;
         overflow-x: auto;
         overflow-y: hidden;
